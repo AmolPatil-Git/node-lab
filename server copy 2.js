9675fdc0d1b591b67549d9b604d605fd7f4bfd7e
Amol Patil <163115830+AmolPatil-Git@users.noreply.github.com>
2025-06-25 00:21:30 +0530

    Remove unused JavaScript files and console log statements

--- server copy 2.js
+++
@@ -1,6 +0,0 @@
-//printing console log
-
-
-console.log("this is syntax of console log")
-var a= hello
-console.log("this is syntax of console log",a)
--- server copy.js
+++
@@ -1,6 +0,0 @@
-//printing console log
-
-
-console.log("this is syntax of console log")
-var a= hello world 
-console.log("this is syntax of console log",a)
